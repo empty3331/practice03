0869dae34d9e402ad57582b746906aa0ec168149
'Ju-Hye' <'empty3331@naver.com'>
2020-05-11 16:21:58 +0900

    while문과 for문 공부4

--- src/com/javaex/practice03/Ex20.java
+++ src/com/javaex/practice03/Ex20.java
@@ -7,41 +7,45 @@ public class Ex20 {
 	public static void main(String[] args) {
 		// 숫자맞추기게임
 		
-		boolean x = true;
-		int num = (int)(Math.random()*100)+1;
-		String yn;
-		String yes = "y";
 		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+		
+		String yn;
+		int num = (int)(Math.random()*100)+1;
+		int n;
+		
+		
+		
 		System.out.println("===============================");
 		System.out.println("     [숫자맞추기게임 시작]     ");
 		System.out.println("===============================");
 		
 		
 
-			while(x) {
+			while(true) {
 		
 				System.out.print(">>");
-				int n = sc.nextInt();
+				n = sc.nextInt();
 				
 				if(n<num) {System.out.println("더높게");}
 				else if(n>num) {System.out.println("더낮게");}
-				else {System.out.println("맞았습니다.");
-					  System.out.print("게임을 종료하시겠습니까?(y/n)>>");
-					  yn = sc.nextLine();
-					  if(yn.equals(yes)) {System.out.println("===============================");
-							             System.out.println("     [숫자맞추기게임 종료]     ");
-							             System.out.println("===============================");}
-					  }
-				
-				System.out.print("");
+				else {System.out.println("맞았습니다.");break;}			
 				
 			}
+			
+			sc.nextLine();
 		
-		
-		
+			System.out.print("게임을 종료하시겠습니까?(y/n)>>");
+			yn = sc.nextLine();
+			if("y".equals(yn)) {
+				System.out.println("===============================");
+			    System.out.println("     [숫자맞추기게임 종료]     ");
+			    System.out.println("===============================");
+			    break;}
+
+	  }
 		
 		sc.close();
-
 	}
-
 }
