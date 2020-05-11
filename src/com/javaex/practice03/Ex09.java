8ae6866de36d13a855f7ac2d3df81d2ec5147f6c
'Ju-Hye' <'empty3331@naver.com'>
2020-05-11 16:06:27 +0900

    while문과 for문 공부4

--- src/com/javaex/practice03/Ex09.java
+++ src/com/javaex/practice03/Ex09.java
@@ -7,85 +7,12 @@ public class Ex09 {
 		int x, y;
 
 
-		x=0;
-
 		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
+			for(x = 0; x<=9; x++) {
+		System.out.print(x+y + "\t");}
 		
-		System.out.println("\n");		
-		
-		
-		x=1;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-			
-		System.out.println("\n");
-			
-			
-		x=2;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
+		System.out.println("");		}
 				
-		System.out.println("\n");	
-				
-		
-		x=3;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-				
-		System.out.println("\n");			
-		
-		
-		x=4;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-				
-		System.out.println("\n");			
-		
-		
-		x=5;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-				
-		System.out.println("\n");			
-		
-		
-		x=6;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-				
-		System.out.println("\n");			
-		
-		
-		x=7;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-				
-		System.out.println("\n");			
-		
-		x=8;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-				
-		System.out.println("\n");			
-		
-		
-		x=9;
-		
-		for(y = 1; y<=10; y++) {
-		System.out.print((x = x +1) + "\t");}
-				
-		System.out.println("\n");			
-		
-
 	}
 
 }
