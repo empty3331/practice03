38e047e753704c80927ec1870c6abe6640d7e51f
'Ju-Hye' <'empty3331@naver.com'>
2020-06-03 19:33:33 +0900

    문제 다시풀기

--- src/com/javaex/practice03/Ex11.java
+++ src/com/javaex/practice03/Ex11.java
@@ -8,32 +8,31 @@ public class Ex11 {
 		
 		//짝수,홀수 덧셈기
 		
-		int i, x, y;
-		int sum =0;
+	int sum = 0;
 		
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("숫자를 입력하세요");
-			i = sc.nextInt();
-			System.out.print("결과값: ");
-			
-			
-			if(i%2==0) {			
-				for(x=1; x<=i; x++) {
-					if(x%2==0) {sum = sum+x;}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 입력하세요");
+		int i = sc.nextInt();
+		
+		
+		if(i%2==0) {
+			for(int x =0; x<=i; x++) {
+				if(x%2==0) {
+					sum = sum+x;}
+			}
+		}else {
+			for(int x = 0; x<=i; x++) {
+				if(!(x%2==0)) {
+					sum = sum+x;
 				}
 			}
-			
-			
-			else {
-				for(y=1; y<=i; y++) {
-					if(y%2==1) {sum = sum+y;}
-				}
-			}
-					
-			System.out.println(sum);
-			
-			sc.close();
 		}
+		
+		System.out.println(sum);
+		
+		
+
+		sc.close();
 	}
 
 }
